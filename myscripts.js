7f643c49caa184ad5097e12415ff2c49d9a67325
shakirahafeez456 <56725618+shakirahafeez456@users.noreply.github.com>
2021-10-13 15:35:46 +0500

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,11 @@
-function btnclicked(){
-    console.log("my button clicked");
+console.log("my first js from an external file");
+var x=5;
+var y=9;
+console.log(x*y);
+function sum(a,b) {
+    return a+b;
 }
+console.log(sum(x,y));
+
+var mysum =sum;
+console.log(mysum);
